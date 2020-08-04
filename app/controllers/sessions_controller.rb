17b2d788151fd2e8935d2fd67a408b5c0d358b5a
Luzaks <kanemekanik@gmail.com>
2020-08-04 14:56:17 -0500

    Feat: Add registration controller with the create method. Fix: Refactor code in sessions controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController 
   def create
-    user = User.find_by(username: params["user"]["username"]).try(:authenticate, ["user"]["password"])
+    user = User.find_by(username: params["user"]["username"]).try(:authenticate, params["user"]["password"])
     if user
-      session[:user_id] = user.user_id
+      session[:user_id] = user.id
       render json: {
         status: :created,
         logged_in: true,
