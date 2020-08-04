4bf2418e26f6c0e0aee941023bbd7d1e3bb3e9b0
Luzaks <kanemekanik@gmail.com>
2020-08-04 15:11:50 -0500

    Feat: Add more registration and sessions features.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController 
+  include CurrentUserConcern
+
   def create
     user = User.find_by(username: params["user"]["username"]).try(:authenticate, params["user"]["password"])
     if user
@@ -13,5 +15,21 @@ class SessionsController < ApplicationController
           status: 401
       }
     end  
+  end
+  def logged_in
+    if @current_user
+      render json: {
+        logged_in: true, 
+        user: @current_user
+      }
+    else
+      render json: {
+        logged_in: false
+      }
+    end
+  end
+  def logout
+    reset_session
+    render json: {  status: 200, logged_out: true }
   end
 end
