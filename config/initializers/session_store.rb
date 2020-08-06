a8025faca581dfc35151f21968b570ce62ca8a81
Luzaks <kanemekanik@gmail.com>
2020-08-06 11:50:48 -0500

    Fix: Change domain name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_tiltzin-api", domain: "https://fierce-taiga-99651.herokuapp.com/"
+  Rails.application.config.session_store :cookie_store, key: "_tiltzin-api", domain: "fierce-taiga-99651.herokuapp.com/"
 else
   Rails.application.config.session_store :cookie_store, key: "_tiltzin-api"
 end
