9ffd1a7b2b080348028e76da744e3d230f2acca1
Luzaks <kanemekanik@gmail.com>
2020-08-10 22:17:58 -0500

    Feat: Add route for manually posting destinies on production.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:create]
   resources :registrations, only: [:create]
   resources :trips, only: [:create]
+  resources :destinies, only: [:create]
   delete :logout, to: "sessions#logout"
   get :logged_in, to: "sessions#logged_in"
   get :index, to: "destinies#index"
