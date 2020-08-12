9efa7ebdb437cdd627bca5aba927e2efd670bb36
Luzaks <kanemekanik@gmail.com>
2020-08-12 09:01:44 -0500

    Fix: Fix last linter errors

--- spec/requests/registrations_spec.rb
+++ spec/requests/registrations_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'registrations actions', type: :request do
-  let(:valid_attributes) { { user: { username: 'John', email: 'john@doe.com', password: 'test123', password_confirmation: 'test123' } } }
+  let(:attributes) { { user: { username: 'John', email: 'j@doe.com', password: '123', password_confirmation: '123' } } }
 
   describe 'POST /registrations' do
-    before { post '/registrations', params: valid_attributes }
+    before { post '/registrations', params: attributes }
 
     it 'tests the username of the created user' do
       expect(json['user']['username']).to eq('John')
     end
 
     it 'tests the email of the created user' do
-      expect(json['user']['email']).to eq('john@doe.com')
+      expect(json['user']['email']).to eq('j@doe.com')
     end
   end
 end
